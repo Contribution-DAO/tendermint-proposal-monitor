9c92c7626dfd6cfc4f9dbb05241a944711345759
wacharakorn.p <sloth.devtaken@gmail.com>
2024-05-17 18:21:24 +0700

    Update(vote): support payload is vote version

--- proposals/proposal_is_vote.go
+++ proposals/proposal_is_vote.go
@@ -7,15 +7,25 @@ import (
 	"tendermint_proposal_monitor/config"
 )
 
-type VoteResponse struct {
+type VoteOption struct {
+	Option string `json:"option"`
+	Weight string `json:"weight"`
+}
+
+type VoteResponseV1 struct {
 	Vote struct {
-		ProposalID string `json:"proposal_id"`
-		Voter      string `json:"voter"`
-		Options    []struct {
-			Option string `json:"option"`
-			Weight string `json:"weight"`
-		} `json:"options"`
-		Metadata string `json:"metadata"`
+		ProposalID string       `json:"proposal_id"`
+		Voter      string       `json:"voter"`
+		Options    []VoteOption `json:"options"`
+		Metadata   string       `json:"metadata"`
+	} `json:"vote"`
+}
+
+type VoteResponseV1Beta1 struct {
+	Vote struct {
+		ProposalID string       `json:"proposal_id"`
+		Voter      string       `json:"voter"`
+		Options    []VoteOption `json:"options"`
 	} `json:"vote"`
 }
 
@@ -31,14 +41,27 @@ func CheckValidatorVoted(chain config.ChainConfig, proposalID string, validatorA
 		return false, nil
 	}
 
-	var voteResponse VoteResponse
-	err = json.NewDecoder(resp.Body).Decode(&voteResponse)
-	if err != nil {
-		return false, fmt.Errorf("error decoding vote response for proposal %s: %w", proposalID, err)
-	}
-
-	if voteResponse.Vote.Voter == validatorAddress {
-		return true, nil
+	switch sdkVersion {
+	case "v1":
+		var voteResponse VoteResponseV1
+		err = json.NewDecoder(resp.Body).Decode(&voteResponse)
+		if err != nil {
+			return false, fmt.Errorf("error decoding vote response for proposal %s: %w", proposalID, err)
+		}
+		if voteResponse.Vote.Voter == validatorAddress {
+			return true, nil
+		}
+	case "v1beta1":
+		var voteResponse VoteResponseV1Beta1
+		err = json.NewDecoder(resp.Body).Decode(&voteResponse)
+		if err != nil {
+			return false, fmt.Errorf("error decoding vote response for proposal %s: %w", proposalID, err)
+		}
+		if voteResponse.Vote.Voter == validatorAddress {
+			return true, nil
+		}
+	default:
+		return false, fmt.Errorf("unsupported sdk version: %s", sdkVersion)
 	}
 
 	return false, nil
